5b24ebe1165de6b572eb2d1bab1ab5f6c08cf897
jherrera <jhan.joshua.h.g@gmail.com>
2024-08-22 14:24:46 -0400

    Se añadio algoritmo de resolucion de la ecuacio para resolver raices cuadradas

---
+++ resolver-funciones-radical/ResolverFuncionesRadical.cpp
@@ -0,0 +1,10 @@
+#include	<iostream>
+#include <math.h>
+
+int main(){
+	double x, resultado;
+	std::cout << "Ingrese	el valor de x: "; 
+	std::cin >> x;
+	resultado = sqrt(abs(pow(x, 3) - 4));
+	std::cout << "El resultado de f(x) es: " << resultado << "\n";
+}
