11137226f4d704b2f6e9f85a8773a2084db92f45
jherrera <jhan.joshua.h.g@gmail.com>
2024-08-22 14:48:22 -0400

    validacion de errores en el codigo

--- resolver-funciones-radical/ResolverFuncionesRadical.cpp
+++ resolver-funciones-radical/ResolverFuncionesRadical.cpp
@@ -1,10 +1,19 @@
 #include	<iostream>
 #include <math.h>
+#include <limits>
 
 int main(){
 	double x, resultado;
-	std::cout << "Ingrese	el valor de x: "; 
-	std::cin >> x;
+	std::cout << "Ingrese	el valor de x: ";
+
+	while (! (std::cin >> x))
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Entrada invalida. Por favor, ingrese un numero: ";
+	}
+
+
 	resultado = sqrt(abs(pow(x, 3) - 4));
 	std::cout << "El resultado de f(x) es: " << resultado << "\n";
 	return 0;
