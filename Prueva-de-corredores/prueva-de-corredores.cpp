f9fc2fb14925cd1f5ce9a2522ca2e1a7803b48ec
jherrera <jhan.joshua.h.g@gmail.com>
2024-08-30 01:32:16 -0400

    se agrego el validador de herrores con limit y el reondeo del promedio

--- Prueva-de-corredores/prueva-de-corredores.cpp
+++ Prueva-de-corredores/prueva-de-corredores.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 #include<vector>
+#include<string.h>
+#include<limits>
+#include<math.h>
 
 using namespace std;
+
+void soloAdmiteNumero(string mensaje) {
+	std::cin.clear();
+	std::cin.ignore(numeric_limits<int>::max(), '\n');
+	std::cout << mensaje;
+}
 
 int main() {
 	// declaraciones de variables
@@ -9,22 +18,29 @@ int main() {
 	float promedioTiempos = 0;
 
 	cout << "Corredores: ";
-	cin >> corredores;
+	while (!(cin >> corredores))
+	{
+		soloAdmiteNumero("Ingres valor numerico: ");
+	}
 
 	// Uso de la libreria vector 
 	vector<int> tiempos(corredores);
 
 	// Proceso
 	cout << " " << endl;
-	cout << "**********************************************" << endl;
-	cout << "Tiempos de los jugadore durante los 100 metros" << endl;
-	cout << "**********************************************" << endl;
+	cout << "*********************************************************" << endl;
+	cout << "Tiempos de los jugadore durante los 100 metros (Segundos)" << endl;
+	cout << "*********************************************************" << endl;
 	cout << " " << endl;
 
 	for (int i = 0; i < corredores; i++)
 	{
 		cout << i + 1 << ':' << " ";
-		cin >> tiempos[i];
+		while (!(cin >> tiempos[i]))
+		{
+			soloAdmiteNumero("Valor numerico (segundos): ");
+		}
+
 		if (tiempos[i] <= 10) corredoresClasificados += 1;
 		promedioTiempos += tiempos[i];
 	}
@@ -39,7 +55,7 @@ int main() {
 	}
 
 	cout << "Clasificados\tPromedio del tiempo" << endl;
-	cout << corredoresClasificados << "\t\t" << (promedioTiempos / corredores) << endl;
+	cout << corredoresClasificados << "\t\t" << round(promedioTiempos / corredores) << endl;
 
 	return 0;
 }
